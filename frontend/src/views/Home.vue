12c48db171d2ebbc61358b41b16418034ae7786f
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-06-07 18:00:38 +0800

    add recaptcha

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,32 @@
-<template>
-  <div class="home">
-    <h1>Cosmos Testnet Faucet</h1>
-      <form v-on:submit.prevent="onSubmit" method="post">
-        <label>Address</label>
-        <input type="text" v-model="address" required>
-        <br>
-        <input type="submit" value="Claim">
-      </form>
-  </div>
+<template lang="pug">
+#home
+  h1 Cosmos Testnet Faucet
+  form(v-on:submit.prevent='onSubmit', method='post')
+    .li-form
+      label(for='faucet-captcha') Captcha
+      vue-recaptcha#faucet-captcha(:sitekey='recaptcha')
+    .li-form
+      label(for='faucet-address') Testnet Address
+      field#faucet-address(type='text', v-model='address', required='', size='lg' placeholder='Your testnet address')
+    .li-form
+      btn(type='submit', value='Claim tokens', size='lg', color='primary')
 </template>
 
 <script>
 import axios from "axios";
+import VueRecaptcha from "vue-recaptcha";
+import Btn from "@nylira/vue-button";
+import Field from "@nylira/vue-field";
 export default {
   name: "home",
-  components: {},
+  components: {
+    Btn,
+    Field,
+    VueRecaptcha
+  },
   data: () => ({
-    address: ""
+    address: "",
+    recaptcha: "6LdqyV0UAAAAAEqgBxvSsDpL2aeTEgkz_VTz1Vi1"
   }),
   methods: {
     onSubmit() {
@@ -40,3 +50,29 @@ export default {
   }
 };
 </script>
+
+<style lang="stylus">
+@import '~variables'
+
+#home
+  max-width 32rem
+  width 100%
+  padding 0 1rem
+  margin 0 auto
+  background var(--app-fg)
+
+  h1
+    font-size 1.25rem
+    font-weight 500
+    padding 1rem 0
+
+  label
+    display none
+
+  .li-form
+    padding 1rem 0
+    border-top 1px solid var(--bc)
+
+  .ni-btn
+    width 100%
+</style>
