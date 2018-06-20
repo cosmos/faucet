8d661ab7a09e68f00548d134b461744bd474ef29
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-06-20 14:21:59 +0800

    add analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
 import Vuelidate from "vuelidate";
+import VueAnalytics from "vue-analytics";
 
 Vue.use(Vuelidate);
-
+Vue.use(VueAnalytics, { id: "UA-51029217-5", router: router });
 Vue.config.productionTip = false;
 
 new Vue({
